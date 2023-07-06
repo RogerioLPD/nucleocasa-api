31711eca64add7511da83e4e97bbbb7596b3dbee
RogerioLPD <rogerio.pinheirodangui@gmail.com>
2023-07-06 13:25:27 -0300

    TODSO OS CORS

--- projeto/settings.py
+++ projeto/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from corsheaders.defaults import default_headers
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -103,16 +104,16 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # CORS_ALLOW_ALL_ORIGINS: True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://nucleocasadecor.com.br",
-    "https://apicasadecor.com:64621",
-    "https://apicasadecor.com:64621",
-    "http://localhost:64621",
-    "http://localhost:8080",
-    "http://localhost:8080",
-    "http://127.0.0.1:64621",
-    "http://127.0.0.1:9000",
-    "http://127.0.0.1:9000",
-    "*",
+    # "https://nucleocasadecor.com.br",
+    # "https://apicasadecor.com:64621",
+    # "https://apicasadecor.com:64621",
+    # "http://localhost:64621",
+    # "http://localhost:8080",
+    # "http://localhost:8080",
+    # "http://127.0.0.1:64621",
+    # "http://127.0.0.1:9000",
+    # "http://127.0.0.1:9000",
+    '*'
 ]
 
 try:
